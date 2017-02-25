11a0d9b8f8997810217572fa0ab9a1b27499259d
Andrew Byrd <Byrd33@live.missouristate.edu>
2017-02-25 14:52:29 -0600

    Add script to rotate table

--- Demo/Assets/TimedInputObject.cs
+++ Demo/Assets/TimedInputObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimedInputObject : MonoBehaviour, TimedInputHandler {
+public class TimedInputObject : TimedInputHandler {
 
 	// Use this for initialization
 	void Start () {
